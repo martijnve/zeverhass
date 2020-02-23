d696e01090e9a8304550bc63f215bbb2f33c8152
martijnve <martijn@vaneijndhoven.net>
2020-02-23 16:49:58 +0100

    Update sensor.py

--- custom_components/zeverhass/sensor.py
+++ custom_components/zeverhass/sensor.py
@@ -1,37 +1,58 @@
-"""
-A sensor platform which detects underruns and capped status from the official Raspberry Pi Kernel.
-Minimal Kernel needed is 4.14+
-"""
+""" Zever solar integration """
+
+VERSION = '0.1.0'
+
+from datetime import date, datetime, timedelta
+import logging
+import requests
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+_LOGGER = logging.getLogger(__name__)
+
+#DEFAULT_NAME = ''
+#DOMAIN = ''
+ICON = 'mdi:wb_sunny'
+#SENSOR_PREFIX = ''
+
+CONST_USERNAME = 'username'
+CONST_PASSWORD = 'password'
+
+SCAN_INTERVAL = timedelta(seconds=30)
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=3600)
+PARALLEL_UPDATES = 1
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONST_USERNAME): cv.string,
+    vol.Required(CONST_PASSWORD): cv.string,
+})
+
 import logging
 import voluptuous as vol
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 
-__version__ = '0.1.5'
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    username = config.get(CONST_USERNAME)
+    password = config.get(CONST_PASSWORD)
 
-_LOGGER = logging.getLogger(__name__)
+    #_LOGGER.debug("Username = %s", username)
 
-SYSFILE = '/sys/devices/platform/soc/soc:firmware/get_throttled'
+    # Setup sensors
+    sensors = []
+    for name in trashTypesDefault:
+        sensors.append(TrashSensor(hass, name, fetch_trash_data, afvaldienst, config))
+    async_add_entities(sensors)
 
-CONF_TEXT_STATE = 'text_state'
+    #_LOGGER.debug("Sensors = %s", sensors)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_TEXT_STATE, default=False): cv.boolean,
-    })
-
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the sensor platform"""
-    import os
-    text_state = config.get(CONF_TEXT_STATE)
-    exist = os.path.isfile(SYSFILE)
-    if exist:
-        add_devices([RaspberryChargerSensor(text_state)], True)
-    else:
-        _LOGGER.critical('Cant find the system class needed for this component, make sure that your kernel is recent and the hardware is supported.')
-
-class RaspberryChargerSensor(Entity):
-    """The class for this sensor"""
+class ZeversolarSensor(Entity):
     def __init__(self, text_state):
         self._state = None
         self._description = None
@@ -39,48 +60,50 @@ class RaspberryChargerSensor(Entity):
         self.update()
 
     def update(self):
-        """The update method"""
-        _throttled = open(SYSFILE, 'r').read()[:-1]
-        _throttled = _throttled[:4]
-        if _throttled == '0':
-            self._description = 'Everything is working as intended'
-        elif _throttled == '1000':
-            self._description = 'Under-voltage was detected, consider getting a uninterruptible power supply for your Raspberry Pi.'
-        elif _throttled == '2000':
-            self._description = 'Your Raspberry Pi is limited due to a bad powersupply, replace the power supply cable or power supply itself.'
-        elif _throttled == '3000':
-            self._description = 'Your Raspberry Pi is limited due to a bad powersupply, replace the power supply cable or power supply itself.'
-        elif _throttled == '4000':
-            self._description = 'The Raspberry Pi is throttled due to a bad power supply this can lead to corruption and instability, please replace your changer and cables.'
-        elif _throttled == '5000':
-            self._description = 'The Raspberry Pi is throttled due to a bad power supply this can lead to corruption and instability, please replace your changer and cables.'
-        elif _throttled == '8000':
-            self._description = 'Your Raspberry Pi is overheating, consider getting a fan or heat sinks.'
+        
+        cookie = 'stationId=; acw_tc=; user=; JSESSIONID='
+
+        now = datetime.datetime.now()
+        month = now.month - 1
+
+        data = fetchForYear(cookie, now.year)
+
+        if data == False:
+          print('unknown')
         else:
+          productionInMonth = float(data[month]['value'][0])
+          print(productionInMonth)
+
+        if False:
             self._description = 'There is a problem with your power supply or system.'
-        if self._text_state:
-            self._state = self._description
-            self._attribute = {'value': _throttled}
-        else:
-            self._state = _throttled
-            self._attribute = {'description': self._description}
+            if self._text_state:
+                self._state = self._description
+                self._attribute = {'value': _throttled}
+            else:
+                self._state = _throttled
+                self._attribute = {'description': self._description}
+            
+    def fetchForYear(cookie, year):
+      headers = {'cookie': cookie}
+      r = requests.get('https://www.zevercloud.com/data/energy/amounty/' + str(year) + '?sid=' + sid + '&isno=' + isno, headers=headers)
+      body = r.content.decode("utf-8")
+      if 'forbidden' in body:
+        return False
+      elif r.status_code == requests.codes.ok:
+        return r.json()[0]['dataset']
 
     @property
     def name(self):
-        """Return the name of the sensor"""
-        return 'RPi Power status'
+        return 'ZeverSolar'
 
     @property
     def state(self):
-        """Return the state of the sensor"""
         return self._state
 
     @property
     def icon(self):
-        """Return the icon of the sensor"""
-        return 'mdi:raspberry-pi'
+        return ICON
 
     @property
     def device_state_attributes(self):
-        """Return the attribute(s) of the sensor"""
         return self._attribute
